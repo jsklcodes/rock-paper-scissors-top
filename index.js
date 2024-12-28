3efca136328328990bcad045d2bbcce7fa72639e
Kallyne Layane <hellookallyne@gmail.com>
2024-12-28 15:53:37 -0300

    Implement logic to obtain the computer's choice

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+const getComputerChoice = () => {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
