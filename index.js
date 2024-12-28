8e5301192bd6fb7e905ed213786e3ebf82ec1f8f
Kallyne Layane <hellookallyne@gmail.com>
2024-12-28 16:08:36 -0300

    Implement logic to obtain the human's choice

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const getComputerChoice = () => {
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
+
+const getHumanChoice = () => {
+  let choice = prompt('Rock, Paper or Scissors?').toLowerCase();
+  const isChoiceInvalid = !['rock', 'paper', 'scissors'].includes(choice);
+
+  while (choice.length === 0 || isChoiceInvalid) {
+    choice = prompt('Enter a valid choice. Rock, Paper or Scissors?');
+  }
+
+  return choice;
+};
