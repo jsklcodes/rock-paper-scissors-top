9d394a26da2e0a4f61bf0c85187b0c2e7ab3d842
Kallyne Layane <hellookallyne@gmail.com>
2024-12-28 16:30:37 -0300

    Implement logic to play a single round

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ const getHumanChoice = () => {
 
 let humanScore = 0;
 let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  const isHumanRoundWinner =
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper');
+
+  if (humanChoice === computerChoice) {
+    console.log(`It's a draw! They both played ${humanChoice}.`);
+  } else if (isHumanRoundWinner) {
+    humanScore++;
+    console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+  }
+};
